d1f2ddb001d7e7c68fd2989794abbf2c632d6be9
akam1o <kamioakira0422@gmail.com>
2022-03-31 01:57:58 +0900

    Update virtctl.py

--- virtctl/virtctl.py
+++ virtctl/virtctl.py
@@ -7,6 +7,27 @@ class VirtCtl:
     def __init__(self):
         pass
 
+    def res_cmd(self, cmd, check=False):
+        return subprocess.run(
+            cmd, stdout=subprocess.PIPE,
+            check=check, shell=True, text=True).stdout
+
+    def res_cmd_lfeed(self, cmd, check=False):
+        return subprocess.run(
+            cmd, stdout=subprocess.PIPE,
+            check=check, shell=True, text=True).stdout.split('\n')
+
+    def yes_no_input(self):
+        while True:
+            if sys.version_info[0] == 3:
+                choice = input("Please respond with 'yes' or 'no' [y/N]: ").lower()
+            else:
+                choice = raw_input("Please respond with 'yes' or 'no' [y/N]: ").lower()
+            if choice in ['y', 'ye', 'yes']:
+                return True
+            elif choice in ['n', 'no']:
+                return False
+
 
 class CommandVirtCtl:
 
